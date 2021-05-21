51e2f780797edd85e8c5be894987cc88ccf70b78
Andaluzov <zhurbasi@gmail.com>
2021-05-21 18:30:59 +0300

    class Deck -  user get random card  from  13  пользователь получает 1 карту случайную из  номинала

---
+++ Blackjack/Deck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Blackjack.Decks
+{
+    public class Deck
+    {
+        int price;
+       
+        const int n = 13;
+        string[] dec = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+        public string GetDeck()
+        {
+            Random a = new Random();
+            string card = dec[a.Next(0, 12)];
+            //Console.WriteLine(card);
+            return card;
+        }
+            
+    }
+}
---
+++ Blackjack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Blackjack.Decks;
+
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Deck deck = new Deck();
+
+            Console.WriteLine(deck.GetDeck());
+        }
+    }
+}
