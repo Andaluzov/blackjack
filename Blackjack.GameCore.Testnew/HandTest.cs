a1db412674e8c2353be677a2328dda7bb2fd8cd7
Andaluzov <zhurbasi@gmail.com>
2021-07-25 14:49:08 +0300

    HandTest resume,  всего 12 тестов, 4 теста HandTest

--- Blackjack.GameCore.Testnew/HandTest.cs
+++ Blackjack.GameCore.Testnew/HandTest.cs
@@ -7,21 +7,14 @@ namespace Blackjack.GameCore.Test
 {
     public class HandTest
     {
-<<<<<<< HEAD
-        private int SearchPlayer(List<int>numbers)
-=======
-        readonly int[] numbers1 = {6, 3, 11};
-        readonly int[] numbers2 = {6, 5, 11};
-
-        [Fact]
-        public void CanGetPoints()
->>>>>>> dee3edb106a21b6803fc45d9cb69e6fb2a050838
+        private int SearchPlayer(List<int> numbers)
         {
             Hand player = new Hand();
             for (int i = 0; i < numbers.Count; i++)
-            player.AddCard(new Card(numbers[i], SuitType.Diamonds));
-            return  player.Points;
+                player.AddCard(new Card(numbers[i], SuitType.Diamonds));
+            return player.Points;
         }
+
         [Fact]
         public void CanGetPoints1()
         {
