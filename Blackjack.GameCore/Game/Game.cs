3f2e2ac11a262c25fcbd01bb4955e9c9558d0ce9
Andaluzov <zhurbasi@gmail.com>
2021-08-19 15:28:53 +0300

    class Game

--- Blackjack.GameCore/Game/Game.cs
+++ Blackjack.GameCore/Game/Game.cs
@@ -19,13 +19,69 @@ namespace Blackjack.GameCore.Game
         }
 
         public void MakeAction(PlayerAction action)
+        // Если значение параметра - “Hit“ (взять еще одну карту), то
+        //a Раздать еще 1 карту из колоды игроку. 
+        //Если колода закончилась - поменять статус игры на “колода закончилась”, 
+        //и вернуться из метода.
+        //b  Проверить, сколько очков у игрока.
+        //Если больше 21 - то игрок проиграл, поменять статус игры на “победа дилера”. 
+        //Иначе - передать ход дилеру (поменять статус игры).
+        //3 Если значение параметра - “Stay”, то карты брать не нужно.
+        //Передать ход дилеру(поменять статус игры).
         {
-
+            if (Status != GameStatus.PlayerTurn)
+            {
+                throw new InvalidOperationException("MakeAction should be called when Game status is PlayerTurn");
+            }
+            else
+            {
+                if (action == PlayerAction.Hit)
+                {
+                    if (!AddCardToHand(PlayerHand))         //выдана 1карта  игроку
+                    {
+                        Status = GameStatus.DeckFinished;
+                       // break; ////
+                    }
+                    else
+                    {
+                        if (PlayerHand.Points >21)
+                        {
+                            Status = GameStatus.DealerWon;
+                        }
+                        else
+                        {
+                            Status = GameStatus.DealerTurn;  ////непонятно, вроде не должен быть переход
+                        }
+                    }
+                };
+                if (action == PlayerAction.Stay)
+                {
+                    Status = GameStatus.DealerTurn;
+                }
+            }
         }
 
         public void MakeDealerMove()
         {
-
+            if (Status != GameStatus.DealerTurn)
+            {
+                throw new InvalidOperationException("MakeDealerMove should be called when Game status is DealerTurn");
+            }
+            else
+            {
+                while(DealerHand.Points < 17) 
+                    //дилер должен тянуть карты до тех пор, пока сумма его очков не будет 17 или больше.
+                    //Если колода закончилась - поменять статус игры на “колода закончилась”, 
+                    //и вернуться из метода.
+                {
+                    if (!AddCardToHand(DealerHand))
+                    {
+                        Status = GameStatus.DeckFinished;
+                        break;
+                    }
+                };
+                GameWinner(); // подсчет очков
+            }
         }
 
         public void DealCards()
@@ -34,51 +90,38 @@ namespace Blackjack.GameCore.Game
             {
                 throw new InvalidOperationException("DealCards should be called when Game status is New");
             }
-
-
-            //else
-            //{ 
-            //    bool dealStatus = !Deck.TryGetCard(out Card _card); //выдана 1карта  дилеру
-            //    if (!dealStatus)
-            //    {
-            //        Status = GameStatus.DeckFinished;
-            //    }
-            //    else
-            //    {
-            //        DealerHand.AddCard(_card);
-            //        Status = GameStatus.DealerTurn;
-            //        dealStatus = !Deck.TryGetCard(out Card _card); //выдана 2карта  дилеру
-            //        if (!dealStatus)
-            //        {
-            //            Status = GameStatus.DeckFinished;
-            //        }
-            //        else
-            //        {
-            //            DealerHand.AddCard(_card);
-            //            Status = GameStatus.PlayerTurn;
-            //        };
-            //        dealStatus = !Deck.TryGetCard(out Card _card); //выдана 1карта  игроку
-            //        if (!dealStatus)
-            //        {
-            //            Status = GameStatus.DeckFinished;
-            //        }
-            //        else
-            //        {
-            //            PlayerHand.AddCard(_card);
-            //            Status = GameStatus.PlayerTurn;
-            //            dealStatus = !Deck.TryGetCard(out Card _card); //выдана 2карта  игроку
-            //            if (!dealStatus)
-            //            {
-            //                Status = GameStatus.DeckFinished;
-            //            }
-            //            else
-            //            {
-            //                PlayerHand.AddCard(_card);
-            //                Status = GameStatus.PlayerTurn;
-            //            }
-            //        }
-            //    }
-            //}
+            else   
+            {               
+                if (!AddCardToHand(DealerHand))      //выдана 1карта  дилеру
+                {
+                    Status = GameStatus.DeckFinished ;
+                }
+                else    
+                {
+                    if (!AddCardToHand(DealerHand))  //выдана 2карта  дилеру
+                    {
+                        Status = GameStatus.DeckFinished;
+                    }
+                    else    
+                    {
+                        if (!AddCardToHand(PlayerHand))         //выдана 1карта  игроку
+                        {
+                            Status = GameStatus.DeckFinished;
+                        }
+                        else   
+                        {
+                            if (!AddCardToHand(PlayerHand))      //выдана 2карта  игроку
+                            {
+                                Status = GameStatus.DeckFinished;
+                            }
+                            else
+                            {
+                                Status = GameStatus.PlayerTurn;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private bool AddCardToHand(Hand hand)
@@ -93,5 +136,65 @@ namespace Blackjack.GameCore.Game
                 return false;
             }
         }
+
+        private  bool GameDeckFinished()
+            {
+                return Status == GameStatus.DeckFinished;
+            }
+
+        private void GameWinner()
+        {
+            //Проверить очки и в зависимости от результата поменять статус игры:
+            //Если у дилера 21, а у игрока нет -дилер победил
+            //Если у обоих 21 - ничья
+            //Если у дилера больше, чем 21 - игрок выиграл
+            //Если у обоих одинаковое количество(не 21) -ничья
+            //Иначе победил тот, у кого больше очков.
+            var player = PlayerHand.Points;
+            var dealer = DealerHand.Points;
+
+            if (dealer == 21)
+            {
+                if (player != 21)
+                {
+                    Status = GameStatus.DealerWon;
+                }
+                else
+                {
+                    if (player == 21)
+                    {
+                        Status = GameStatus.Equal;
+                    }
+                }
+            }
+            else
+            {
+                if(dealer > 21)
+                {
+                    Status = GameStatus.PlayerWon;
+                }
+                else
+                {
+                    if(dealer == player)
+                    {
+                        Status = GameStatus.Equal;
+                    }
+                    else
+                    {
+                        if(dealer > player)
+                        {
+                            Status = GameStatus.DealerWon;
+                        }
+                        else 
+                        {
+                            Status = GameStatus.PlayerWon;
+                        }
+                    }
+                }
+            }
+
+        }
+            
+        
     }
 }
